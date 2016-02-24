80b18f7c0f823c1698afb251672ec2edc05a47c4
alexpapster <alexpap@cs.brown.edu>
2016-02-24 16:54:27 -0500

    Documentation for util.js

--- src/util.js
+++ src/util.js
@@ -4,15 +4,14 @@ window.gazer = window.gazer || {};
 gazer.util = gazer.util || {};
 gazer.mat = gazer.mat || {};
 
-//Type test functions
-gazer.util.isInt;
-
-gazer.util.isNaN;
-
-gazer.util.isString;
-
-//Eye class
-
+/**
+ * Eye class, represents an eye patch detected in the video stream
+ * @param {ImageData} patch - the image data corresponding to an eye
+ * @param {number} imagex - x-axis offset from the top-left corner of the video canvas
+ * @param {number} imagey - y-axis offset from the top-left corner of the video canvas
+ * @param {number} width  - width of the eye patch
+ * @param {number} height - height of the eye patch
+ */
 gazer.util.Eye = function(patch, imagex, imagey, width, height) {
     this.patch = patch;
     this.imagex = imagex;
@@ -22,27 +21,45 @@ gazer.util.Eye = function(patch, imagex, imagey, width, height) {
 }
 
 
-//Data Window class
-//operates like an array but 'wraps' data around to keep the array at a fixed windowSize
-gazer.util.DataWindow = function(windowSize) {
+/**
+ * DataWindow class - Operates like an array, but 'wraps' data around to keep the array at a fixed windowSize
+ * @param {number} windowSize - defines the maximum size of the window
+ * @param {data} [data] - optional data to seed the DataWindow with
+**/
+gazer.util.DataWindow = function(windowSize, data) {
     this.data = [];
     this.windowSize = windowSize;
     this.index = 0;
     this.length = 0;
+    if(array){
+        this.data = data.slice(data.length-windowSize,data.length);
+        this.length = this.data.length;
+    }
 }
 
+/**
+ * [push description]
+ * @param  {Any} entry - item to be inserted. It either grows the DataWindow or replaces the oldest item
+ * @return {DataWindow} this
+ */
 gazer.util.DataWindow.prototype.push = function(entry) {
     if (this.data.length < this.windowSize) {
         this.data.push(entry);
         this.length = this.data.length;
-        return entry;
+        return this;
     }
 
-    //replace oldest entry
+    //replace oldest entry by wrapping around the DataWindow
     this.data[this.index] = entry;
     this.index = (this.index + 1) % this.windowSize;
+    return this;
 }
 
+/**
+ * Get the element at the ind position by wrapping around the DataWindow
+ * @param  {number} ind index of desired entry
+ * @return {Any} 
+ */
 gazer.util.DataWindow.prototype.get = function(ind) {
     if (this.data.length < this.windowSize) {
         return this.data[ind];
@@ -52,8 +69,11 @@ gazer.util.DataWindow.prototype.get = function(ind) {
     }
 }
 
+/**
+ * Append all the contents of data
+ * @param {array} data - to be inserted 
+ */
 gazer.util.DataWindow.prototype.addAll = function(data) {
-    //TODO use slice instead?
     for (var i = 0; i < data.length; i++) {
         this.push(data[i]);
     }
@@ -61,11 +81,26 @@ gazer.util.DataWindow.prototype.addAll = function(data) {
 
 
 //Helper functions
+
+/**
+ * Grayscales an image patch. Can be used for the whole canvas, detected face, detected eye, etc.
+ * @param  {ImageData} imageData - image data to be grayscaled
+ * @param  {number} imageWidth  - width of image data to be grayscaled
+ * @param  {number} imageHeight - height of image data to be grayscaled
+ * @return {ImageData} grayscaledImage 
+ */
 gazer.util.grayscale = function(imageData, imageWidth, imageHeight){
     //TODO either move tracking into gazer namespace or re-implement function
     return tracking.Image.grayscale(imageData, imageWidth, imageHeight, false);
 }
 
+/**
+ * Gets an Eye object and resizes it to the desired resolution
+ * @param  {gazer.util.Eye} eye - patch to be resized
+ * @param  {number} resizeWidth - desired width
+ * @param  {number} resizeHeight - desired height
+ * @return {gazer.util.Eye} resized eye patch
+ */
 gazer.util.resizeEye = function(eye, resizeWidth, resizeHeight) {
 
     //TODO this seems like it could be done in just one painting to a canvas
@@ -81,7 +116,7 @@ gazer.util.resizeEye = function(eye, resizeWidth, resizeHeight) {
     tempCanvas.width = resizeWidth;
     tempCanvas.height = resizeHeight;
 
-    // save your canvas into temp canvas
+    // save the canvas into temp canvas
     tempCanvas.getContext('2d').drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, resizeWidth, resizeHeight);
 
     return tempCanvas.getContext('2d').getImageData(0, 0, resizeWidth, resizeHeight);
