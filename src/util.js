2cc095b9a63b3df971ebb277dcb504427caf70e8
Tristan Valcke <valcketristan@gmail.com>
2016-11-09 13:54:51 +0100

    Add/Update jsdoc to utils objects methods

--- src/util.js
+++ src/util.js
@@ -8,10 +8,10 @@
     /**
      * Eye class, represents an eye patch detected in the video stream
      * @param {ImageData} patch - the image data corresponding to an eye
-     * @param {number} imagex - x-axis offset from the top-left corner of the video canvas
-     * @param {number} imagey - y-axis offset from the top-left corner of the video canvas
-     * @param {number} width  - width of the eye patch
-     * @param {number} height - height of the eye patch
+     * @param {Number} imagex - x-axis offset from the top-left corner of the video canvas
+     * @param {Number} imagey - y-axis offset from the top-left corner of the video canvas
+     * @param {Number} width  - width of the eye patch
+     * @param {Number} height - height of the eye patch
      */
     self.webgazer.util.Eye = function(patch, imagex, imagey, width, height) {
         this.patch = patch;
@@ -26,8 +26,8 @@
     //operates like an array but 'wraps' data around to keep the array at a fixed windowSize
     /**
      * DataWindow class - Operates like an array, but 'wraps' data around to keep the array at a fixed windowSize
-     * @param {number} windowSize - defines the maximum size of the window
-     * @param {data} [data] - optional data to seed the DataWindow with
+     * @param {Number} windowSize - defines the maximum size of the window
+     * @param {Array} data - optional data to seed the DataWindow with
      **/
     self.webgazer.util.DataWindow = function(windowSize, data) {
         this.data = [];
@@ -42,7 +42,7 @@
 
     /**
      * [push description]
-     * @param  {Any} entry - item to be inserted. It either grows the DataWindow or replaces the oldest item
+     * @param  {*} entry - item to be inserted. It either grows the DataWindow or replaces the oldest item
      * @return {DataWindow} this
      */
     self.webgazer.util.DataWindow.prototype.push = function(entry) {
@@ -60,8 +60,8 @@
 
     /**
      * Get the element at the ind position by wrapping around the DataWindow
-     * @param  {number} ind index of desired entry
-     * @return {Any}
+     * @param  {Number} ind index of desired entry
+     * @return {*}
      */
     self.webgazer.util.DataWindow.prototype.get = function(ind) {
         return this.data[this.getTrueIndex(ind)];
@@ -69,8 +69,8 @@
 
     /**
      * Gets the true this.data array index given an index for a desired element
-     * @param {number} ind - index of desired entry
-     * @return {number} index of desired entry in this.data
+     * @param {Number} ind - index of desired entry
+     * @return {Number} index of desired entry in this.data
      */
     self.webgazer.util.DataWindow.prototype.getTrueIndex = function(ind) {
         if (this.data.length < this.windowSize) {
@@ -83,7 +83,7 @@
 
     /**
      * Append all the contents of data
-     * @param {array} data - to be inserted
+     * @param {Array} data - to be inserted
      */
     self.webgazer.util.DataWindow.prototype.addAll = function(data) {
         for (var i = 0; i < data.length; i++) {
@@ -96,8 +96,8 @@
     /**
      * Grayscales an image patch. Can be used for the whole canvas, detected face, detected eye, etc.
      * @param  {ImageData} imageData - image data to be grayscaled
-     * @param  {number} imageWidth  - width of image data to be grayscaled
-     * @param  {number} imageHeight - height of image data to be grayscaled
+     * @param  {Number} imageWidth  - width of image data to be grayscaled
+     * @param  {Number} imageHeight - height of image data to be grayscaled
      * @return {ImageData} grayscaledImage
      */
     self.webgazer.util.grayscale = function(imageData, imageWidth, imageHeight){
@@ -108,8 +108,8 @@
     /**
      * Increase contrast of an image
      * @param {ImageData} grayscaleImageSrc - grayscale integer array
-     * @param {number} step - sampling rate, control performance
-     * @param {array} destinationImage - array to hold the resulting image
+     * @param {Number} step - sampling rate, control performance
+     * @param {Array} destinationImage - array to hold the resulting image
      */
     self.webgazer.util.equalizeHistogram = function(grayscaleImageSrc, step, destinationImage) {
         //TODO implement ourselves to remove dependency
@@ -119,8 +119,8 @@
     /**
      * Gets an Eye object and resizes it to the desired resolution
      * @param  {webgazer.util.Eye} eye - patch to be resized
-     * @param  {number} resizeWidth - desired width
-     * @param  {number} resizeHeight - desired height
+     * @param  {Number} resizeWidth - desired width
+     * @param  {Number} resizeHeight - desired height
      * @return {webgazer.util.Eye} resized eye patch
      */
     self.webgazer.util.resizeEye = function(eye, resizeWidth, resizeHeight) {
@@ -147,8 +147,8 @@
 
     /**
      * Checks if the prediction is within the boundaries of the viewport and constrains it
-     * @param  {array} prediction [x,y] predicted gaze coordinates
-     * @return {array} constrained coordinates
+     * @param  {Array} prediction [x,y] - predicted gaze coordinates
+     * @return {Array} constrained coordinates
      */
     self.webgazer.util.bound = function(prediction){
         if(prediction.x < 0)
@@ -168,6 +168,11 @@
         return prediction;
     }
 
+    /**
+     * Write statistics in debug paragraph panel
+     * @param {HTMLElement} para - The <p> tag where write data
+     * @param {Object} stats - The stats data to output
+     */
     function debugBoxWrite(para, stats) {
         var str = '';
         for (var key in stats) {
@@ -176,6 +181,12 @@
         para.innerText = str;
     }
 
+    /**
+     * Constructor of DebugBox object,
+     * it insert an paragraph inside a div to the body, in view to display debug data
+     * @param {Number} interval - The log interval
+     * @constructor
+     */
     self.webgazer.util.DebugBox = function(interval) {
         this.para = document.createElement('p');
         this.div = document.createElement('div');
@@ -193,10 +204,22 @@
         }(this));
     }
 
+    /**
+     * Add stat data for log
+     * @param {String} key - The data key
+     * @param {*} value - The value
+     */
     self.webgazer.util.DebugBox.prototype.set = function(key, value) {
         this.stats[key] = value;
     }
 
+    /**
+     * Initialize stats in case where key does not exist, else
+     * increment value for key
+     * @param {String} key - The key to process
+     * @param {Number} incBy - Value to increment for given key (default: 1)
+     * @param {Number} init - Initial value in case where key does not exist (default: 0)
+     */
     self.webgazer.util.DebugBox.prototype.inc = function(key, incBy, init) {
         if (!this.stats[key]) {
             this.stats[key] = init || 0;
@@ -204,6 +227,11 @@
         this.stats[key] += incBy || 1;
     }
 
+    /**
+     * Create a button and register the given function to the button click event
+     * @param {String} name - The button name to link
+     * @param {Function} func - The onClick callback
+     */
     self.webgazer.util.DebugBox.prototype.addButton = function(name, func) {
         if (!this.buttons[name]) {
             this.buttons[name] = document.createElement('button');
@@ -215,6 +243,12 @@
         button.innerText = name;
     }
 
+    /**
+     * Search for a canvas elemenet with name, or create on if not exist.
+     * Then send the canvas element as callback parameter.
+     * @param {String} name - The canvas name to send/create
+     * @param {Function} func - The callback function where send canvas
+     */
     self.webgazer.util.DebugBox.prototype.show = function(name, func) {
         if (!this.canvas[name]) {
             this.canvas[name] = document.createElement('canvas');
@@ -230,12 +264,12 @@
      * Kalman filters work by reducing the amount of noise in a models.
      * https://blog.cordiner.net/2011/05/03/object-tracking-using-a-kalman-filter-matlab/
      *
-     * @param {array of arrays} F  		-> transition matrix
-     * @param {array of arrays} Q		  -> process noise matrix
-     * @param {array of arrays} H 		-> maps between measurement vector and noise matrix
-     * @param {array of arrays} R     -> defines measurement error of the device
-     * @param {array}           P_initial -> the initial state
-     * @param {array}           X_initial -> the initial state of the device
+     * @param {Array.<Array.<Number>>} F - transition matrix
+     * @param {Array.<Array.<Number>>} Q - process noise matrix
+     * @param {Array.<Array.<Number>>} H - maps between measurement vector and noise matrix
+     * @param {Array.<Array.<Number>>} R - defines measurement error of the device
+     * @param {Array} P_initial - the initial state
+     * @param {Array} X_initial - the initial state of the device
      */
     self.webgazer.util.KalmanFilter = function(F, H, Q, R, P_initial, X_initial) {
         this.F = F; // State transition matrix
@@ -249,8 +283,8 @@
 
     /**
      * Get Kalman next filtered value and update the internal state
-     * @param {array} z  	-> the new measurement
-     * @return {array}
+     * @param {Array} z - the new measurement
+     * @return {Array}
      */
     self.webgazer.util.KalmanFilter.prototype.update = function(z) {
 
