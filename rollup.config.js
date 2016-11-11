841128181c461cee1de11c3eda329afdbe8127f1
Tristan VALCKE <tristan.valcke@arturia.com>
2016-11-11 21:52:01 +0100

    Add babel plugin for rollup in view to transiple es6 (backward compat) in package.json and in rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,18 @@
+import babel from 'rollup-plugin-babel';
 
 export default {
-    entry: './src/webgazer.js',
-    format: 'iife',
-    plugins: [],
-    dest: './build/webgazer_roll.js'
+    entry:      'src/webgazer.js',
+    dest:       'build/webgazer.js',
+    format:     'umd',
+    moduleName: 'WebGazer',
+    sourceMap:  true,
+    plugins:    [
+        babel({
+            presets: [
+                ["es2015", { "modules": false }]
+            ],
+            babelrc: false,
+            exclude: ['node_modules/**']
+        })
+    ]
 };
