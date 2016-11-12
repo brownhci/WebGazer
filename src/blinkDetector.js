d45625da5c2afe054e839201f420cb8f9b2141b9
Tristan VALCKE <tristan.valcke@arturia.com>
2016-11-12 21:04:25 +0100

    Break namespacing (bad practice !) for BlinkDetector, and set it as module, update intendation due to modularization

--- src/blinkDetector.js
+++ src/blinkDetector.js
@@ -1,64 +1,60 @@
-(function(window) {
-    "use strict";
-    
-    window.webgazer = window.webgazer || {};
 
-    //TODO
-    /**
-     * Constructor for BlinkDetector
-     * @param blinkWindow
-     * @constructor
-     */
-    webgazer.BlinkDetector = function(blinkWindow) {
-        //TODO use DataWindow instead
-        this.blinkData = [];
-        //determines number of previous eyeObj to hold onto
-        this.blinkWindow = blinkWindow || 8;
+/**
+ * Constructor for BlinkDetector
+ * @param blinkWindow
+ * @constructor
+ */
+function BlinkDetector(blinkWindow) {
+    //TODO use DataWindow instead
+    this.blinkData = [];
+    //determines number of previous eyeObj to hold onto
+    this.blinkWindow = blinkWindow || 8;
 
-        //cycles through to replace oldest entry
-        this.blinkWindowIndex = 0;
-    };
+    //cycles through to replace oldest entry
+    this.blinkWindowIndex = 0;
+}
 
-    //TODO
-    /**
-     *
-     * @param eyesObj
-     * @returns {*}
-     */
-    webgazer.BlinkDetector.prototype.detectBlink = function(eyesObj) {
-        if (!eyesObj) {
-            return eyesObj;
-        }
-        if (this.blinkData.length < this.blinkWindow) {
-            this.blinkData.push(eyesObj);
-            eyesObj.left.blink = false;
-            eyesObj.right.blink = false;
-            return eyesObj;
-        }
+BlinkDetector.constructor = BlinkDetector;
 
-        //replace oldest entry
-        this.blinkData[this.blinkWindowIndex] = eyesObj;
-        this.blinkWindowIndex = (this.blinkWindowIndex + 1) % this.blinkWindow;
-
-        //TODO detect if current eyeObj is different from eyeObj in blinkData;
-
+//TODO
+/**
+ *
+ * @param eyesObj
+ * @returns {*}
+ */
+BlinkDetector.prototype.detectBlink = function(eyesObj) {
+    if (!eyesObj) {
+        return eyesObj;
+    }
+    if (this.blinkData.length < this.blinkWindow) {
+        this.blinkData.push(eyesObj);
         eyesObj.left.blink = false;
         eyesObj.right.blink = false;
         return eyesObj;
-    };
-
-    //TODO
-    /**
-     *
-     * @param value
-     * @returns {webgazer.BlinkDetector}
-     */
-    webgazer.BlinkDetector.prototype.setBlinkWindow = function(value) {
-        if (webgazer.utils.isInt(value) && value > 0) {
-            this.blinkWindow = value;
-        }
-        return this;
     }
 
-}(window));
+    //replace oldest entry
+    this.blinkData[this.blinkWindowIndex] = eyesObj;
+    this.blinkWindowIndex = (this.blinkWindowIndex + 1) % this.blinkWindow;
 
+    //TODO detect if current eyeObj is different from eyeObj in blinkData;
+
+    eyesObj.left.blink = false;
+    eyesObj.right.blink = false;
+    return eyesObj;
+};
+
+//TODO
+/**
+ *
+ * @param value
+ * @returns {webgazer.BlinkDetector}
+ */
+BlinkDetector.prototype.setBlinkWindow = function(value) {
+    if (webgazer.utils.isInt(value) && value > 0) {
+        this.blinkWindow = value;
+    }
+    return this;
+};
+
+export { BlinkDetector };
