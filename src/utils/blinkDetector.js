04eb30c0dda5bc3fc5fe422ce16cb8ae68fceb6c
Tristan VALCKE <tristan.valcke@arturia.com>
2016-11-16 02:52:59 +0100

    Split base webgazer folder into sub-directory accordingly to the es6 module pattern

--- src/utils/blinkDetector.js
+++ src/utils/blinkDetector.js
@@ -14,42 +14,30 @@ function BlinkDetector(blinkWindow) {
     this.blinkWindowIndex = 0;
 }
 
-BlinkDetector.constructor = BlinkDetector;
+BlinkDetector.prototype.constructor = BlinkDetector;
 
-//TODO
-/**
- *
- * @param eyesObj
- * @returns {*}
- */
 BlinkDetector.prototype.detectBlink = function(eyesObj) {
-    if (!eyesObj) {
-        return eyesObj;
-    }
-    if (this.blinkData.length < this.blinkWindow) {
-        this.blinkData.push(eyesObj);
+        if (!eyesObj) {
+            return eyesObj;
+        }
+        if (this.blinkData.length < this.blinkWindow) {
+            this.blinkData.push(eyesObj);
+            eyesObj.left.blink = false;
+            eyesObj.right.blink = false;
+            return eyesObj;
+        }
+
+        //replace oldest entry
+        this.blinkData[this.blinkWindowIndex] = eyesObj;
+        this.blinkWindowIndex = (this.blinkWindowIndex + 1) % this.blinkWindow;
+
+        //TODO detect if current eyeObj is different from eyeObj in blinkData;
+
         eyesObj.left.blink = false;
         eyesObj.right.blink = false;
         return eyesObj;
-    }
+    };
 
-    //replace oldest entry
-    this.blinkData[this.blinkWindowIndex] = eyesObj;
-    this.blinkWindowIndex = (this.blinkWindowIndex + 1) % this.blinkWindow;
-
-    //TODO detect if current eyeObj is different from eyeObj in blinkData;
-
-    eyesObj.left.blink = false;
-    eyesObj.right.blink = false;
-    return eyesObj;
-};
-
-//TODO
-/**
- *
- * @param value
- * @returns {webgazer.BlinkDetector}
- */
 BlinkDetector.prototype.setBlinkWindow = function(value) {
     if (webgazer.utils.isInt(value) && value > 0) {
         this.blinkWindow = value;
