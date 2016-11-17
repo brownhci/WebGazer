d7083d2b405464b681317df301d871ce920567dc
Tristan Valcke <valcketristan@gmail.com>
2016-11-17 18:20:38 +0100

    Add a dependencies wrapper to allow correct dependencies compilation under es6

---
+++ dependencies/dependencies_wrapper.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tvalcke on 17/11/2016.
+ */
+
+
+// export {objectdetect as ObjectDetect};
+// export {tracking as Tracking};
+// export {numeric as Numeric};
+
+export {supports_video as supportsVideo};
+export {supports_h264_baseline_video as supportsH264BaselineVideo};
+export {supports_ogg_theora_video as supportsOggTheoraVideo};
