42a5b604314a6c89dd26767c12dbd35912e10024
TristanValcke <valcke.tristan@gmail.com>
2016-11-18 20:54:15 +0100

    Add in right place comment to keep in mind files dependencies

--- dependencies/dependencies_wrapper.js
+++ dependencies/dependencies_wrapper.js
@@ -2,11 +2,44 @@
  * Created by tvalcke on 17/11/2016.
  */
 
-
+// 'dependencies/js-objectdetect/js/objectdetect.js',
+// 'dependencies/js-objectdetect/js/objectdetect.eye.js  ',
+// 'dependencies/js-objectdetect/js/objectdetect.frontalface_alt.js  ',
 // export {objectdetect as ObjectDetect};
+
+// 'dependencies/tracking.js/build/tracking.js',
+// 'dependencies/tracking.js/build/data/face-min.js',
+// 'dependencies/tracking.js/build/data/eye-min.js',
 // export {tracking as Tracking};
+
+// 'dependencies/numeric/numeric-1.2.6.js',
 // export {numeric as Numeric};
 
-export {supports_video as supportsVideo};
-export {supports_h264_baseline_video as supportsH264BaselineVideo};
-export {supports_ogg_theora_video as supportsOggTheoraVideo};
+// 'dependencies/clmtrackr/utils.js',
+// export {supports_video as supportsVideo};
+// export {supports_h264_baseline_video as supportsH264BaselineVideo};
+// export {supports_ogg_theora_video as supportsOggTheoraVideo};
+
+// 'dependencies/clmtrackr/mosse.js',
+// export {mosseFilter as mosseFilter};
+// export {FFT as fastFourierTransformation};
+
+
+
+// 'dependencies/clmtrackr/frontalface.js',
+
+// 'dependencies/clmtrackr/jsfeat-min.js',
+// 'dependencies/clmtrackr/jsfeat_detect.js',
+// 'dependencies/clmtrackr/left_eye_filter.js',
+// 'dependencies/clmtrackr/right_eye_filter.js',
+// 'dependencies/clmtrackr/nose_filter.js',
+// 'dependencies/clmtrackr/model_pca_20_svm.js',
+// 'dependencies/clmtrackr/clm.js',
+// 'dependencies/clmtrackr/svmfilter_webgl.js',
+// 'dependencies/clmtrackr/svmfilter_fft.js',
+// 'dependencies/clmtrackr/mossefilter.js'
+
+
+
+
+
