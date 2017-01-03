9e1a951d5f78cb6b83034438fa197858c8e9e37c
Tristan VALCKE <tristan.valcke@gmail.com>
2017-01-03 16:09:28 +0100

    LinearReg, comment return doc to fix warning, and object refactoring (waiting better change)

--- src/regression/linearReg.js
+++ src/regression/linearReg.js
@@ -1,4 +1,93 @@
 import * as Core from "../core/core";
+//
+// /**
+//  * LinearReg class which uses pupil detection and simple linear regression to predict gaze from eye patches
+//  * @alias module:LinearReg
+//  * @exports LinearReg
+//  * @constructor
+//  */
+// function LinearReg() {
+//
+//     this.name = 'simple';
+//
+//     this.leftDatasetX  = [];
+//     this.leftDatasetY  = [];
+//     this.rightDatasetX = [];
+//     this.rightDatasetY = [];
+//     this._data          = [];
+// }
+//
+// Object.assign( LinearReg.prototype, {
+//
+//     addData: function (eyes, screenPos, type) {
+//         if (!eyes) {
+//             return;
+//         }
+//
+//         Core.getPupils(eyes);
+//         if (!eyes.left.blink) {
+//             this.leftDatasetX.push([eyes.left.pupil[0][0], screenPos[0]]);
+//             this.leftDatasetY.push([eyes.left.pupil[0][1], screenPos[1]]);
+//         }
+//
+//         if (!eyes.right.blink) {
+//             this.rightDatasetX.push([eyes.right.pupil[0][0], screenPos[0]]);
+//             this.rightDatasetY.push([eyes.right.pupil[0][1], screenPos[1]]);
+//         }
+//         this._data.push({'eyes': eyes, 'screenPos': screenPos, 'type': type});
+//     },
+//
+//     setData:function (data) {
+//         for (var i = 0; i < data.length; i++) {
+//             this.addData(data[i].eyes, data[i].screenPos, data[i].type);
+//         }
+//         this._data = data;
+//     },
+//
+//     getData:function () {
+//         return this._data;
+//     },
+//
+//     predict: function (eyesObj) {
+//         if (!eyesObj) {
+//             return null;
+//         }
+//
+//         var result          = regression('linear', this.leftDatasetX);
+//         var leftSlopeX      = result.equation[0];
+//         var leftIntersceptX = result.equation[1];
+//
+//         result              = regression('linear', this.leftDatasetY);
+//         var leftSlopeY      = result.equation[0];
+//         var leftIntersceptY = result.equation[1];
+//
+//         result               = regression('linear', this.rightDatasetX);
+//         var rightSlopeX      = result.equation[0];
+//         var rightIntersceptX = result.equation[1];
+//
+//         result               = regression('linear', this.rightDatasetY);
+//         var rightSlopeY      = result.equation[0];
+//         var rightIntersceptY = result.equation[1];
+//
+//         Core.getPupils(eyesObj);
+//
+//         var leftPupilX = eyesObj.left.pupil[0][0];
+//         var leftPupilY = eyesObj.left.pupil[0][1];
+//
+//         var rightPupilX = eyesObj.right.pupil[0][0];
+//         var rightPupilY = eyesObj.right.pupil[0][1];
+//
+//         var predictedX = Math.floor((((leftSlopeX * leftPupilX) + leftIntersceptX) + ((rightSlopeX * rightPupilX) + rightIntersceptX)) / 2);
+//         var predictedY = Math.floor((((leftSlopeY * leftPupilY) + leftIntersceptY) + ((rightSlopeY * rightPupilY) + rightIntersceptY)) / 2);
+//         return {
+//             x: predictedX,
+//             y: predictedY
+//         };
+//     }
+//
+// } );
+//
+//////////////////////////////////////////
 
 /**
  * LinearReg class which uses pupil detection and simple linear regression to predict gaze from eye patches
@@ -81,8 +170,8 @@ LinearReg.prototype.getData = function () {
  * after apply linear regression on data set
  * @param {Object} eyesObj - The current user eyes object
  * @return {Object} prediction - Object containing the prediction data
- *  @return {integer} prediction.x - the x screen coordinate predicted
- *  @return {integer} prediction.y - the y screen coordinate predicted
+// *  @return {integer} prediction.x - the x screen coordinate predicted
+// *  @return {integer} prediction.y - the y screen coordinate predicted
  */
 LinearReg.prototype.predict = function (eyesObj) {
 
