b418a8c9acb8ea4b8d61b512ef5f6aafdb6e43ab
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2017-04-20 16:22:06 +1200

    add pop up msg after clibration process

--- js/calibration.js
+++ js/calibration.js
@@ -1,9 +1,17 @@
+var PointCalibrate = 0;
 $(document).ready(function() {
    $("input[type='button']").click(function(){
       $(this).css('background-color','red');
+      PointCalibrate++;
+      if (PointCalibrate == 9){
+        swal("Successfully calibrate all points");
+      }
+      $(this).prop('disabled', true);
+
     });
     swal("Please start the calibration process by clicking on the points");
 });
+
 
 
 
