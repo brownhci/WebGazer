061f619d6c72f419a183090aeba2e137abe9de62
Ida <ides542@aucklanduni.ac.nz>
2017-04-27 11:58:19 +1200

    got the buttons to dissappear and a popup

--- js/calibration.js
+++ js/calibration.js
@@ -5,9 +5,17 @@ $(document).ready(function() {
       PointCalibrate++;
       if (PointCalibrate == 9){
         swal("Successfully calibrate all points");
+        for(count = 1; count < 10; count++){ // loops through all buttons & hides them
+          if (count != 5){
+            var name = 'Pt' + count;
+            document.getElementById(name).style.visibility = 'hidden';
+          }
+        }
+        var canvas = document.getElementById("plotting_canvas"); // clears canvas of points clicked
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        swal("Pleas stare at the middle dot for the next 5 seconds as we calculate the precision");
       }
       $(this).prop('disabled', true);
-
     });
     swal("Please start the calibration process by clicking on the points");
 });
@@ -18,10 +26,3 @@ function ClearCalibration(){
   var canvas = document.getElementById("plotting_canvas");
   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 }
-
-
-
-
-
-
-
