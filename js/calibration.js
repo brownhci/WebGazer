7c073234d24dd0bbcf628f3c7a5253d76e6e1a1a
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2017-04-27 15:25:10 +1200

    user has to click on the calibration 5 times before the point is calibrated

--- js/calibration.js
+++ js/calibration.js
@@ -1,26 +1,35 @@
 var PointCalibrate = 0;
+var CalibrationPoints={};
+
 $(document).ready(function() {
-   $("input[type='button']").click(function(){
-      $(this).css('background-color','red');
-      PointCalibrate++;
-      if (PointCalibrate == 9){
-        swal("Successfully calibrate all points");
+    $(".Calibration").click(function(){
+      
+      var id = $(this).attr('id');
+      console.log(id);
+      if (!CalibrationPoints[id]){
+        CalibrationPoints[id]=0;
       }
-      $(this).prop('disabled', true);
+      
+      CalibrationPoints[id]++;
+      console.log(CalibrationPoints[id]);
+      if (CalibrationPoints[id]>=5){
+        $(this).css('background-color','red');
+        PointCalibrate++;
+        if (PointCalibrate == 9){
+          swal("Successfully calibrate all points");
+        }
+        $(this).prop('disabled', true);
+      }
+      
 
     });
     swal("Please start the calibration process by clicking on the points");
 });
 function ClearCalibration(){
   $(".Calibration").css('background-color','yellow');
+  $(".Calibration").prop('disabled',false);
+  CalibrationPoints = {};
   PointCalibrate = 0;
   var canvas = document.getElementById("plotting_canvas");
   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 }
-
-
-
-
-
-
-
