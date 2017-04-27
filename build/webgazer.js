e2ea054410fc07ee99ec4eafff95c3451b28efcf
Ida <ides542@aucklanduni.ac.nz>
2017-04-28 11:32:02 +1200

    added comments

--- build/webgazer.js
+++ build/webgazer.js
@@ -10356,10 +10356,16 @@ var mosseFilterResponses = function() {
 
 var draw_points = false;
 
+/**
+* This makes the variable draw_points true, so all the previous prediction points are plotted on the canvas
+*/
 function draw_points_variable(){
   draw_points = true;
 }
 
+/**
+* This makes the variable draw_points false, so points aren't plotted on the canvas
+*/
 function stop_drawing_points_variable(){
   draw_points = false;
 }
--- js/calibration.js
+++ js/calibration.js
@@ -1,6 +1,10 @@
 var PointCalibrate = 0;
 var CalibrationPoints={};
 
+/**
+* This function listens for button clicks on the html page
+* checks that all buttons have been clicked 5 times each, and then goes on to measuring the precision
+*/
 $(document).ready(function() {
     $(".Calibration").click(function(){
 
@@ -12,32 +16,37 @@ $(document).ready(function() {
 
       CalibrationPoints[id]++;
       console.log(CalibrationPoints[id]);
-      if (CalibrationPoints[id]>=5){
+      if (CalibrationPoints[id]>=5){ //only turnns red after 5 clicks
         $(this).css('background-color','red');
-        $(this).prop('disabled', true);
+        $(this).prop('disabled', true); //disables the button
         PointCalibrate++;
-          if (PointCalibrate == 9){
+
+          if (PointCalibrate == 9){ // last point is calibrated
             for(count = 1; count < 10; count++){ // loops through all buttons & hides them
               if (count != 5){
                 var name = 'Pt' + count;
                 document.getElementById(name).style.visibility = 'hidden';
               }
             }
-            var canvas = document.getElementById("plotting_canvas"); // clears canvas of points clicked
+            // clears the canvas
+            var canvas = document.getElementById("plotting_canvas");
             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+            // notification for the measurement process
             swal({
               title: "Calculating measurement",
               text: "Please stare at the middle dot for the next 5 seconds as we calculate the precision",
               allowEscapeKey: false,
               allowOutsideClick: false,
-              closeOnConfirm: true
+              closeOnConfirm: true,
+              timer: 4000
             }, function(isConfirm){
               if (isConfirm){
+                // makes the variables true for 5 seconds & plots the points
                 $(document).ready(function(){
-                  draw_points_variable();
+                  draw_points_variable(); //  starts drawing prediction points on the canvas
                   sleep(5000).then(() => {
-                      stop_drawing_points_variable();
-                      //libby calculates accuracy here
+                      stop_drawing_points_variable(); //  stops drawing prediction points on the canvas
+
                       swal({
                         title: "Your accuracy measure is 55%",
                         showCancelButton: true,
@@ -73,6 +82,7 @@ function ClearCalibration(){
   var canvas = document.getElementById("plotting_canvas");
   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 }
+// sleep function because java doesn't have one, sourced from http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
