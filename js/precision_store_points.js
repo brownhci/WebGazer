27bddeac1c1f18b89035ffbc69a7200a94fb86fb
Libby713 <e_stevenson@xtra.co.nz>
2017-04-28 16:36:43 +1200

    Created precision_store_points so that points are only stored when user is staring at point for 5 seconds. Called in calibration. Omadny

---
+++ js/precision_store_points.js
@@ -0,0 +1,16 @@
+var store_points_var = false;
+
+/**
+* This makes the variable store_points true, so all the occuring prediction
+* points are stored
+*/
+function store_points_variable(){
+  store_points_var = true;
+}
+
+/**
+* This makes the variable store_points false, so points aren't stored any more
+*/
+function stop_storing_points_variable(){
+  store_points_var = false;
+}
