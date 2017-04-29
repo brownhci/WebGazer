f6e5677f224cb6e0a572d0cbea12da1f0af460a7
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2017-04-29 14:45:00 +1200

    Gradually increase the opacity of the calibration points when clicked. It turns to yellow when the point is done.

--- js/calibration.js
+++ js/calibration.js
@@ -19,11 +19,16 @@ $(document).ready(function() {
       CalibrationPoints[id]++;
       console.log(CalibrationPoints[id]);
       if (CalibrationPoints[id]==5){ //only turnns red after 5 clicks
-        $(this).css('background-color','red');
+        
+        $(this).css('background-color','yellow');
         $(this).prop('disabled', true); //disables the button
         PointCalibrate++;
 
           
+      }else if (CalibrationPoints[id]<5){
+        //Gradually increase the opacity of calibration points when click to give some indication to user.
+        var opacity = 0.2*CalibrationPoints[id]+0.2;
+        $(this).css('opacity',opacity);
       }
       //Show the middle calibration point after all other points have been clicked.
       if (PointCalibrate == 8){
@@ -74,11 +79,14 @@ $(document).ready(function() {
     });
     swal({
       title: "Calibration",
-      text: "Please click on the each of the 9 points on the screen. You must click each point 5 times till it goes red. This will calibrate your eye movements."
+      text: "Please click on the each of the 9 points on the screen. You must click each point 5 times till it goes yellow. This will calibrate your eye movements.",
+      allowOutsideClick: false,
+      showConfirmButton: true
     });
 });
 function ClearCalibration(){
-  $(".Calibration").css('background-color','yellow');
+  $(".Calibration").css('background-color','red');
+  $(".Calibration").css('opacity',0.2);
   $(".Calibration").prop('disabled',false);
 
   CalibrationPoints = {};
