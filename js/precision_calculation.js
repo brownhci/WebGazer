de964b6fcfbfffadf183bf8eccd4f06054a83aec
Libby713 <e_stevenson@xtra.co.nz>
2017-05-04 17:46:31 +1200

    Updated precision calculation to be more precise. Omadny

--- js/precision_calculation.js
+++ js/precision_calculation.js
@@ -14,44 +14,41 @@ function calculatePrecision(past50Array) {
   var staringPointX = windowWidth / 2;
   var staringPointY = windowHeight / 2;
 
-  var xAverage = 0;
-  var yAverage = 0;
+  var precisionPercentages = new Array(50);
 
   /*
-   * find the average distances from the position of the staring point
+   * calculate percentage accuracy for each prediction based on distance of
+   * the prediction point from the centre point (uses the window height as
+   * lower threshold 0%)
    */
-  //sum predictions
   for (x = 0; x < 50; x++) {
-    xAverage += x50[x];
-    yAverage += y50[x];
+    //calculate distance between each prediction and staring point
+    var xDiff = staringPointX - x50[x];
+    var yDiff = staringPointY - y50[x];
+    var distance = Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
+
+    //calculate precision percentage
+     var halfWindowHeight = windowHeight / 2;
+     var precision = 0;
+     if (distance <= halfWindowHeight && distance > -1) {
+       precision = 100 - (distance / halfWindowHeight * 100);
+     } else if (distance > halfWindowHeight) {
+       precision = 0;
+     } else if (distance > -1) {
+       precision = 100;
+     }
+
+     //store the precision
+     precisionPercentages[x] = precision;
   }
 
-  //divide predictions to get averages
-  xAverage = xAverage / 50;
-  yAverage = yAverage / 50;
-
-  /*
-   * calculate distance between average and staring point
-   */
-  var xDiff = staringPointX - xAverage;
-  var yDiff = staringPointY - yAverage;
-  var distance = Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
-
-  /*
-   * calculate percentage accuracy based on distance of the average prediction
-   * point from the centre point (uses the window height as lower threshold 0%)
-   */
-   var halfWindowHeight = windowHeight / 2;
-   var precision = 0;
-   if (distance <= halfWindowHeight && distance > -1) {
-     precision = 100 - (distance / halfWindowHeight * 100);
-     precision = Math.round(precision);
-   } else if (distance > halfWindowHeight) {
-     precision = 0;
-   } else if (distance > -1) {
-     precision = 100;
-   }
+  //find the average of all precision percentages calculated
+  var precision = 0;
+  for (x = 0; x < 50; x++) {
+    precision += precisionPercentages[x];
+  }
+  precision = precision / 50;
 
    //return the precision measurement (percentage)
-   return precision;
+   return Math.round(precision);
 };
