48fcede9a4ff47e940e496461a16c1817d16535f
idaknow <ides542@aucklanduni.ac.nz>
2017-11-28 15:24:00 +1300

    added documentation in index.html

--- build/webgazer.js
+++ build/webgazer.js
@@ -11173,19 +11173,3 @@ function stop_drawing_points_variable(){
 }(window));
 ;
 
-var toAlert = true;
-
-/**
-* This changes the boolean for whether the user if notified with a popup about lighting
-*/
-function setAlert(){
-  if (toAlert){
-    toAlert = false;
-  } else {
-    toAlert = true;
-  }
-}
-
-window.setTimeout(setAlert, 20000); // occurs every 20secs
-;
-
