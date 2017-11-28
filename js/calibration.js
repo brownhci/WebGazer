49833a0c5f34b13e081d6173d4934f748ec83654
idaknow <ides542@aucklanduni.ac.nz>
2017-11-28 15:16:58 +1300

    fixed spacing in code

--- js/calibration.js
+++ js/calibration.js
@@ -9,6 +9,7 @@ function ClearCanvas(){
   var canvas = document.getElementById("plotting_canvas");
   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 }
+
 /**
  * Show the instruction of using calibration at the start up screen.
  */
@@ -25,13 +26,13 @@ function PopUpInstruction(){
       }
     });
 }
+
 /**
   * Show the help instructions right at the start.
   */
 function helpModalShow() {
     $('#helpModal').modal('show');
 }
-
 
 /**
  * Load this function when the index page starts.
@@ -48,25 +49,23 @@ $(document).ready(function(){
       if (!CalibrationPoints[id]){ // initialises if not done
         CalibrationPoints[id]=0;
       }
-
       CalibrationPoints[id]++; // increments values
 
       if (CalibrationPoints[id]==5){ //only turnns red after 5 clicks
-
         $(this).css('background-color','yellow');
         $(this).prop('disabled', true); //disables the button
         PointCalibrate++;
-
-
       }else if (CalibrationPoints[id]<5){
         //Gradually increase the opacity of calibration points when click to give some indication to user.
         var opacity = 0.2*CalibrationPoints[id]+0.2;
         $(this).css('opacity',opacity);
       }
+
       //Show the middle calibration point after all other points have been clicked.
       if (PointCalibrate == 8){
         $("#Pt5").show();
       }
+
       if (PointCalibrate >= 9){ // last point is calibrated
             //using jquery to grab every element in Calibration class and hide them except the middle point.
             $(".Calibration").hide();
@@ -124,13 +123,13 @@ $(document).ready(function(){
           }
     });
 });
+
 /**
  * Show the Calibration Points
  */
 function ShowCalibrationPoint() {
   $(".Calibration").show();
   $("#Pt5").hide(); // initially hides the middle button
-
 }
 
 /**
@@ -143,10 +142,9 @@ function ClearCalibration(){
   $(".Calibration").prop('disabled',false);
 
   CalibrationPoints = {};
-
   PointCalibrate = 0;
-
 }
+
 // sleep function because java doesn't have one, sourced from http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
