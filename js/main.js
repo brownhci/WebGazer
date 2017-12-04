4a3bf0d378dd2c6231479217898f11d37a8294f2
idaknow <ides542@aucklanduni.ac.nz>
2017-12-05 10:00:52 +1300

    added console statements back into the code

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ window.onload = function() {
     webgazer.setRegression('ridge') /* currently must set regression and tracker */
         .setTracker('clmtrackr')
         .setGazeListener(function(data, clock) {
+          //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+          //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
         })
         .begin()
         .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
