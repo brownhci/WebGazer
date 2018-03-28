47c58ad780bcc4b11c76d145f7aacccae5877b80
Ida <ides542@aucklanduni.ac.nz>
2018-03-28 17:12:48 -0400

    seperated the precision files to webgazer and imports to HMTL

--- www/webgazer.js
+++ www/webgazer.js
@@ -10374,6 +10374,21 @@ var mosseFilterResponses = function() {
 
 }());
 
+/*
+ * Initialises variables used by calibration example file
+ */
+var store_points_var = false;
+var xPast50 = new Array(50);
+var yPast50 = new Array(50);
+
+/*
+ * Stores the position of the fifty most recent tracker preditions
+ */
+function store_points(x, y, k) {
+  xPast50[k] = x;
+  yPast50[k] = y;
+}
+
 (function(window, undefined) {
     console.log('initializing webgazer');
     //strict mode for type safety
