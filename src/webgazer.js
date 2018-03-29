e2b4920b637a7b98fcd80e77457119fd1992abfe
Ida <ides542@aucklanduni.ac.nz>
2018-03-29 10:29:32 -0400

    removed deprecated URL object

--- src/webgazer.js
+++ src/webgazer.js
@@ -395,12 +395,7 @@
         videoElement.style.display = 'none';
 
         // set the video source as the stream
-        if ("srcObject" in videoElement) {
-          videoElement.srcObject = videoStream;
-        } else {
-          // used for older browsers
-          videoElement.src = window.URL.createObjectURL(videoStream);
-        }
+        videoElement.srcObject = videoStream;
 
         document.body.appendChild(videoElement);
 
