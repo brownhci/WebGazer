8d0ed7fe76d51fe3386cb77b3c0ca684ab84bc3a
robinganemccalla@gmail.com <robinganemccalla@gmail.com>
2020-08-18 13:38:56 -0400

    Add tests of basic webgazer functionality, enable getting video feed and additional tests in www_tests

--- test/webgazer_function_tests.js
+++ test/webgazer_function_tests.js
@@ -3,13 +3,30 @@ const { expect, assert } = require('chai');
 const TFFaceMesh = require('@tensorflow-models/facemesh');
 const common = require('./common');
 
+
+function startTimer() {
+   const time = process.hrtime();
+   return time;
+ }
+
+ function endTimer(time) {
+   const diff = process.hrtime(time);
+   const NS_PER_SEC = 1e9;
+   const result = (diff[0] * NS_PER_SEC + diff[1]); // Result in Nanoseconds
+   return elapsed = result * 0.001;
+ }
+
+
 it('top level, non-video no arguments webgazer functions should work', async() =>{
 		await common.page.setDefaultNavigationTimeout(60000);
+		let time = startTimer()
 		let basic_functions = await common.page.evaluate(async() => {
 			while (document.getElementById('wgGP').style.height == ""){
 				await new Promise(r => setTimeout(r, 1000));
 			}
+			debugger
 			let pred = await webgazer.getCurrentPrediction()
+			
 			return {getCurrentPrediction: JSON.stringify(await webgazer.getCurrentPrediction()),
 					getEyeFeats: JSON.stringify(await webgazer.util.getEyeFeats(pred.eyeFeatures)),
 					addMouseEventListeners: JSON.stringify(await webgazer.addMouseEventListeners()),
@@ -22,24 +39,41 @@ it('top level, non-video no arguments webgazer functions should work', async() =
 					clearGazeListener:JSON.stringify(await webgazer.clearGazeListener()),
 					getRegression:JSON.stringify(await webgazer.getRegression()),
 					//getEventTypes:JSON.stringify(await webgazer.getEventTypes()), -needs params
-					//getVideoElementCanvas:JSON.stringify(await webgazer.getVideoElementCanvas()),
-					//getVideoPreviewToCameraResolutionRatio:JSON.stringify(await webgazer.getVideoPreviewToCameraResolutionRatio()),
+					//
 					getStoredPoints:JSON.stringify(await webgazer.getStoredPoints()),
 					pause:JSON.stringify(await webgazer.pause())
 				}
-		})
+			})
+		console.log("took ",endTimer(time)," nanoseconds")
 		//can use to ensure prediction is accurately displayed on page, just need to do some math
 		//and get variables from page
 		debugger
-		let wgGP_location = await common.page.evaluate(async() => {
-			return {left:document.getElementById('wgGP').style.left,
-					top:document.getElementById('wgGP').style.top}
-				})
+		// let wgGP_location = await common.page.evaluate(async() => {
+		// 	return {left:document.getElementById('wgGP').style.left,
+		// 			top:document.getElementById('wgGP').style.top}
+		// 		})
 		for(const [k,v] of Object.entries(basic_functions)){ 
   			assert.notEqual(Object.keys(v),null)
-  			assert.notEqual(Object.keys(v),null)
+  			assert.notEqual(Object.keys(v),{})
   			}
 
 		assert.equal(basic_functions.isReady,"true")
 		assert.equal(basic_functions.detectCompatibility,"true")
-	}) 
+	})
+it('can record screen position, set tracker and regression and set static video', async() =>{
+	const screen_functions = common.page.evaluate(async() => {
+		return {setStaticVideo: await webgazer.setStaticVideo('../www/data/src/P_02/1491487691210_2_-study-dot_test_instructions.webm'),
+				recordScreenPosition: await webgazer.recordScreenPosition(100, 100), 
+				recordScreenPositionAction: await webgazer.recordScreenPosition(100, 100, 'mouseclick'), 
+				storePoints: await webgazer.storePoints(100, 100, 0), 
+				setTracker: await webgazer.setTracker('TFFacemesh'), 
+				setRegression: await webgazer.setRegression('ridge')}
+	})
+	for(const [k,v] of Object.entries(screen_functions)){ 
+  			assert.notEqual(Object.keys(v),null)
+  			assert.notEqual(Object.keys(v),{})
+  			}
+})
+
+
+
--- test/www_page_test.js
+++ test/www_page_test.js
@@ -1,11 +1,20 @@
 const puppeteer = require('puppeteer');
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 const TFFaceMesh = require('@tensorflow-models/facemesh');
 
 describe('Page Basics', async () => {
 	let browser,page,doc;
 	before(async () => {
-		browser = await puppeteer.launch();
+		//convert .webm to mp4 https://cloudconvert.com/webm-to-mp4
+		//convert mp4 to y4m with:
+		//ffmpeg -i 1491487691210_2_-study-dot_test_instructions.webm -pix_fmt yuv420p dot.y4m
+		//sed -i '0,/C420mpeg2/s//C420/' *.y4m
+		//give absolute path!
+		let my_y4m_video = '/home/robin/workspace/WebGazer/www/data/src/P_02/dot.y4m'
+		browser = await puppeteer.launch({args:['--use-file-for-fake-video-capture='+my_y4m_video,
+		'--allow-file-access', '--use-fake-device-for-media-stream','--use-fake-ui-for-media-stream',
+		'--no-sandbox','--disable-setuid-sandbox']
+		,devtools:true });
 		page = await browser.newPage();
   		await page.goto('http://localhost:3000');
 	})
@@ -22,11 +31,38 @@ describe('Page Basics', async () => {
 		})
   	});
 
+  	it('clicking the collision button should send you to new page' , async() =>{
+		
+  		const collision_button = "#collision_button";
+		const [response] = await Promise.all([
+		  page.waitForNavigation(),
+		  page.click(collision_button),
+		]);
+		expect(page.url()).to.equal('http://localhost:3000/collision.html?')
+		expect(response.status()).to.equal(200)
+	});
   	it('clicking the calibration button should send you to new page' , async() =>{
+  		await page.goto('http://localhost:3000');
   		const calibration_button = "#calibration_button";
 		await page.click(calibration_button);
 		expect(page.url()).to.equal('http://localhost:3000/calibration.html?')
 	});
+	//
+	it('should be able to recognize video input', async() =>{
+		await page.waitForSelector("#start_calibration")
+		await page.evaluate(async() => {
+			document.querySelector("#start_calibration").click()
+		})
+		await page.waitForSelector("body > div.swal-overlay.swal-overlay--show-modal > div > div.swal-footer > div > button")
+		await page.evaluate(async() =>{
+			document.querySelector("body > div.swal-overlay.swal-overlay--show-modal > div > div.swal-footer > div > button").click()
+		})
+		const videoAvailable = await page.evaluate(async() => {
+			return await webgazer.params.showFaceFeedbackBox;
+		});
+		console.log(videoAvailable)
+		// debugger
+		assert.equal(videoAvailable,true);
 
 	it('clicking the collision button should send you to new page' , async() =>{
 		await page.goto('http://localhost:3000');
@@ -35,3 +71,36 @@ describe('Page Basics', async () => {
 		expect(page.url()).to.equal('http://localhost:3000/collision.html?')
 	});
 });
+
+// webgazer.begin(()=>{})
+// return webgazer instance - errors?
+// webgazer.showVideo(true)
+// return webgazer instance
+// webgazer.showFaceOverlay(true)
+// return webgazer instance 
+// //can we simulate a webcam or do we have to use the video?
+// //how can we simulate webcam input for puppeteer?
+// //https://stackoverflow.com/questions/52464583/possible-to-get-puppeteer-audio-feed-and-or-input-audio-directly-to-puppeteer
+// webgazer.showFaceFeedbackBox(true) 
+// webgazer.showPredictionPoints(true)
+					//getVideoElementCanvas:JSON.stringify(await webgazer.getVideoElementCanvas()),
+					//getVideoPreviewToCameraResolutionRatio:JSON.stringify(await webgazer.getVideoPreviewToCameraResolutionRatio()),
+					//checkEyesInValidationBox
+//setVideoViewerSize: await webgazer.setVideoViewerSize(640, 480),
+
+
+
+// webgazer.setStaticVideo('../www/data/src/P_02/1491487691210_2_-study-dot_test_instructions.webm')
+// returns webgazer instance 
+// webgazer.setVideoViewerSize(640, 480)
+// //returns nothing 
+// webgazer.recordScreenPosition(100, 100) 
+// webgazer.recordScreenPosition(100, 100, 'mouseclick') 
+// webgazer.storePoints(100, 100, 0) 
+// webgazer.setTracker('TFFacemesh') 
+// webgazer.setRegression('ridge') 
+// webgazer.addTrackerModule(name, constructor) 
+// webgazer.addRegressionModule(name, constructor) 
+// webgazer.addRegression(name) 
+// webgazer.setGazeListener(listener) 
+// webgazer.setVideoElementCanvas(canvas - get from page) 
