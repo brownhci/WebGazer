4a4416960568a24f0a16d6f950dbc7b865666a87
robinganemccalla@gmail.com <robinganemccalla@gmail.com>
2020-08-28 13:14:40 -0400

    Fix netstat comptability issue for OSX

--- test/run_tests_and_server.sh
+++ test/run_tests_and_server.sh
@@ -2,15 +2,16 @@
 if  ! [ -f www/data/src/P_01/dot.y4m ] ; then
 	echo "Creating .y4m file for video input"
 	ffmpeg -hide_banner -loglevel panic -i www/data/src/P_01/1491423217564_2_-study-dot_test_instructions.webm -pix_fmt yuv420p dot.y4m > /dev/null
-	sed -i '0,/C420mpeg2/s//C420/' dot.y4m
+	sed -i.bak '0,/C420mpeg2/s//C420/' dot.y4m
 	mv dot.y4m www/data/src/P_01/
+	rm dot.y4m.bak
 fi
-if ! netstat -ltnp | grep :8000 ; then
+if ! lsof -i:8000 ; then
 	cd www/data/src
 	python3 webgazerExtractServer.py > /dev/null &
 	cd ../../..
 fi
-if ! netstat -ltnp | grep :3000 ; then
+if ! lsof -i:3000 ; then
 	cd www/
 	browser-sync start --server --no-open > /dev/null &
 	cd ..
