7f2dab4798d1ffc416570ae8def45a098448162e
Jeff Huang <github@jeffhuang.com>
2023-01-18 02:08:51 -0500

    update to webpack 5 which also makes this compatible with node 18
    addresses #287 and #273

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,85 @@
 const path = require('path');
 const webpack = require('webpack');
-const createVariants = require('parallel-webpack').createVariants;
+const TerserPlugin = require('terser-webpack-plugin');
 
 const bannerString =`
  WebGazer.js: Scalable Webcam EyeTracking Using User Interactions
- Copyright (c) 2016-2023, Brown HCI Group
+ Copyright (c) 2016, Brown HCI Group
  Licensed under GPLv3. Companies with a valuation of less than $1M can use WebGazer.js under LGPLv3.
  `;
 
-function createConfig(options) {
-  return {
-    entry: './src/index.mjs',
-    output: {
-      filename: 'webgazer' + 
-		(options.target == 'var' ? '' : '.' + options.target) + 
-		(options.minified ? '.min' : '') + 
-		'.js',
-      library: 'webgazer',
-      libraryTarget: options.target,
-      libraryExport: 'default',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.mjs$/,
-          type: 'javascript/auto',
-          exclude: /node_modules/
-        }
-      ]
-    },
-    optimization: {
-    	minimize: options.minified
-    },
-    resolve: {
-      extensions: [".mjs", ".webpack.js", ".web.js", ".js", ".json"]
-    },
-    plugins: [
-      new webpack.BannerPlugin(bannerString),
-    ],
-    devtool: "source-map"
-  };
-}
-module.exports = createVariants({
-  minified: [true, false],
-  target: ['var','commonjs2']
-}, createConfig);
+const varConfig = {
+	entry: './src/index.mjs',
+	output: {
+		filename: 'webgazer.js',
+		library: {
+			name: 'webgazer',
+			type: 'var',
+			export: 'default',
+		},
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module:	{
+		rules: [
+			{
+				test: /\.mjs$/,
+				type: 'javascript/auto',
+				exclude: /node_modules/,
+				resolve: {
+					fullySpecified: false,
+				},
+			}
+		]
+	},
+	resolve: {
+		extensions: [".mjs", ".webpack.js", ".web.js", ".js", ".json"]
+	},
+	optimization: {
+		minimizer: [new TerserPlugin({
+			extractComments: false,
+		})],
+	},
+	plugins: [
+		new webpack.BannerPlugin(bannerString),
+	],
+	devtool: "source-map"
+};
+
+const commonjs2Config = {
+	entry: './src/index.mjs',
+	output: {
+		filename: 'webgazer.commonjs2.js',
+		library: {
+			name: 'webgazer',
+			type: 'commonjs2',
+			export: 'default',
+		},
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module:	{
+		rules: [
+			{
+				test: /\.mjs$/,
+				type: 'javascript/auto',
+				exclude: /node_modules/,
+				resolve: {
+					fullySpecified: false,
+				},
+			}
+		]
+	},
+	resolve: {
+		extensions: [".mjs", ".webpack.js", ".web.js", ".js", ".json"]
+	},
+	optimization: {
+		minimizer: [new TerserPlugin({
+			extractComments: false,
+		})],
+	},
+	plugins: [
+		new webpack.BannerPlugin(bannerString),
+	],
+	devtool: "source-map"
+};
+
+module.exports = [varConfig, commonjs2Config]
