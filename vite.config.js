f11286eb9d53abe40da692030e5f656009d53a37
Manoel Dantas <manoel.lobo@gmail.com>
2024-09-11 22:49:14 -0300

    stop copying built lib to www folder

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,12 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import banner from 'vite-plugin-banner'
-import { copyFileSync } from 'node:fs'
 
 const bannerString = `
  WebGazer.js: Democratizing Webcam Eye Tracking on the Browser
  Copyright (c) 2016, Brown WebGazer Team
  Licensed under GPLv3. Companies with a valuation of less than $1M can use WebGazer.js under LGPLv3.
  `
-
-/**
- * Make a copy of the build bundle to www folder
- */
- const copyToWww = () =>{
-  return {
-    name: 'copy-to-www',
-    apply: 'build',
-    writeBundle() {
-      copyFileSync(resolve(__dirname, 'dist/webgazer.js'), resolve(__dirname, 'www/webgazer.js'))
-      copyFileSync(resolve(__dirname, 'dist/webgazer.js.map'), resolve(__dirname, 'www/webgazer.js.map'))
-      copyFileSync(resolve(__dirname, 'dist/webgazer.js'), resolve(__dirname, 'www/data/src/webgazer.js'))
-      copyFileSync(resolve(__dirname, 'dist/webgazer.js.map'), resolve(__dirname, 'www/data/src/webgazer.js.map'))
-    }
-  }
-}
 
 export default defineConfig({
   build: {
@@ -34,5 +17,5 @@ export default defineConfig({
     },
     sourcemap: true
   },
-  plugins: [banner(bannerString), copyToWww()]
+  plugins: [banner(bannerString)]
 })
